4aea8fc9c3b3ea1964ab3f32e3c6e9c762b3c718
N4M11 <naminatadiakite66@gmail.com>
2025-10-06 11:40:59 +0200

    get de client app ok

--- client-app/src/main/java/com/mycompany/client/app/ClientApp.java
+++ client-app/src/main/java/com/mycompany/client/app/ClientApp.java
@@ -3,24 +3,47 @@
  */
 
 package com.mycompany.client.app;
-import com.rgb.training.app.data.model.Reparation;
+
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
 /**
  *
  * @author nami
  */
-
 public class ClientApp {
+    
+    private final static String URL = "http://localhost:8080/training-app/api/jta/mytable";
+    
+    public static List<MyTable> getAll(Client client) {
+        try {
+            return client.target(URL)
+                        .request(MediaType.APPLICATION_JSON)
+                        .get(new GenericType<List<MyTable>>() {});
+        } catch (Exception e) {
+            System.out.println("ERROR <getAll>: " + e.getMessage());
+            return null;
+        }
+    }
 
     public static void main(String[] args) {
+        Client client = ClientBuilder.newClient();
         
-       Client client = ClientBuilder.newClient();
-       
-        Reparation name = client.target("http://example.com/webapi/hello")
-            .request(MediaType.APPLICATION_JSON)
-        .get(Reparation.class);
-        
+        try {
+            List<MyTable> all = getAll(client);
+            
+            System.out.println("-- GET ALL MY TABLE --");
+            for (MyTable mt : all) {
+                System.out.println(mt);
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            client.close();
+        }
     }
 }
---
+++ client-app/src/main/java/com/mycompany/client/app/MyTable.java
@@ -0,0 +1,126 @@
+package com.mycompany.client.app;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author LuisCarlosGonzalez
+ */
+@Entity
+@Table(name = "my_table")
+@XmlType(name = "MyTable")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MyTable implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+    @Column(name = "birthdate")
+    @Temporal(TemporalType.DATE)
+    private Date birthdate;
+    @Size(max = 255)
+    @Column(name = "address", length = 255)
+    private String address;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "enabled", nullable = false)
+    private boolean enabled;
+
+    public MyTable() {
+    }
+
+    public MyTable(String name, Date birthdate, String address, boolean enabled) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MyTable)) {
+            return false;
+        }
+        MyTable other = (MyTable) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MyTable{" + "id=" + id + ", name=" + name + ", birthdate=" + birthdate + ", address=" + address + ", enabled=" + enabled + '}';
+    }
+}
